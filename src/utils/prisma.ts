e68fdf73b3c66584a312fc5bd83681cd2fc59f1b
Charlie Chiu <tvz292@gmail.com>
2026-05-18 16:46:52 +0800

    fix: force explicit DATABASE_URL injection for Prisma

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,5 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL environment variable is not defined');
+}
+
+const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL,
+        },
+    },
+} as any);
 
 export default prisma;
