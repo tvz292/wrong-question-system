31ef06cdada1c8e4d35c24af27694b04cf07febe
Charlie Chiu <tvz292@gmail.com>
2026-05-18 16:55:29 +0800

    fix: simplify prisma client and ensure dotenv config

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,7 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-} as any);
+const prisma = new PrismaClient();
 
 export default prisma;
